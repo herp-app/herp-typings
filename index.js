714d1554deb08601eb9250a1044bf34863d05126
Johannes Hoffmann <hoppe@juhujuhu.de>
2020-10-30 12:19:23 +0100

    import changes

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// /// <reference path="./IBundle.ts" />
+// /// <reference path="./IConfiguration.ts" />
+// /// <reference path="./IEntity.ts" />
+// /// <reference path="./IFields.ts" />
+// /// <reference path="./IForm.ts" />
+// /// <reference path="./ILayout.ts" />
+// /// <reference path="./IMenu.ts" />
+// /// <reference path="./IPermissions.ts" />
+// /// <reference path="./IQuery.ts" />
+// /// <reference path="./IService.ts" />
+// /// <reference path="./ITranslation.ts" />
+// /// <reference path="./IView.ts" />
+// /// <reference path="./IWorkflows.ts" />
+// /// <reference path="./Utils.ts" />
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,60 @@
 
-declare module 'herp-typings';
 
-/// <reference path="./IBundle.ts" />
-/// <reference path="./IConfiguration.ts" />
-/// <reference path="./IEntity.ts" />
-/// <reference path="./IFields.ts" />
-/// <reference path="./IForm.ts" />
-/// <reference path="./ILayout.ts" />
-/// <reference path="./IMenu.ts" />
-/// <reference path="./IPermissions.ts" />
-/// <reference path="./IQuery.ts" />
-/// <reference path="./IService.ts" />
-/// <reference path="./ITranslation.ts" />
-/// <reference path="./IView.ts" />
-/// <reference path="./IWorkflows.ts" />
-/// <reference path="./Utils.ts" />
+// /// <reference path="./IBundle.ts" />
+// /// <reference path="./IConfiguration.ts" />
+// /// <reference path="./IEntity.ts" />
+// /// <reference path="./IFields.ts" />
+// /// <reference path="./IForm.ts" />
+// /// <reference path="./ILayout.ts" />
+// /// <reference path="./IMenu.ts" />
+// /// <reference path="./IPermissions.ts" />
+// /// <reference path="./IQuery.ts" />
+// /// <reference path="./IService.ts" />
+// /// <reference path="./ITranslation.ts" />
+// /// <reference path="./IView.ts" />
+// /// <reference path="./IWorkflows.ts" />
+// /// <reference path="./Utils.ts" />
 
+
+// declare module 'herp-typings';
+
+import IFields, { FieldsCollectionType, IFieldsConfiguration, FieldsConfigurationCollectionType } from "./IFields";
+
+interface IEntityBase {
+    type: "entity";
+    bundle: string;
+    name: string;
+    label: string;
+    description: string;
+    options?: any;
+}
+
+/**
+ * Interface for a Entity
+ */
+export interface IEntityConfiguration extends IEntityBase {
+    fields: FieldsConfigurationCollectionType;
+}
+
+
+/**
+ * Interface for a Entity
+ */
+export default interface IEntity extends IEntityBase {
+    fields: FieldsCollectionType;
+}
+
+/**
+ * System entity IEntityUser - need by permission and logging
+ */
+export interface IEntityUser {
+    email: string;
+    password: string;
+    roles: Array<string>;
+    _id: string;
+}
+
+export interface IEntityReference {
+    bundle: string;
+    entity: string;
+}
