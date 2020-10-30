9f41318a7784909fa972debd1ae7470c57b58424
Johannes Hoffmann <hoppe@juhujuhu.de>
2020-10-30 12:57:37 +0100

    export all field types correctly

--- dist/IFields.d.ts
+++ dist/IFields.d.ts
@@ -9,6 +9,17 @@ import { IFieldPassword, IFieldPasswordConfiguration } from "./fields/IFieldPass
 import { IFieldRelation, IFieldRelationConfiguration } from "./fields/IFieldRelation";
 import { IFieldSerial, IFieldSerialConfiguration } from "./fields/IFieldSerial";
 import { IFieldString, IFieldStringConfiguration } from "./fields/IFieldString";
+export * from "./fields/IFieldBlock";
+export * from "./fields/IFieldBoolean";
+export * from "./fields/IFieldDate";
+export * from "./fields/IFieldDateTime";
+export * from "./fields/IFieldDocument";
+export * from "./fields/IFieldEnumerate";
+export * from "./fields/IFieldNumber";
+export * from "./fields/IFieldPassword";
+export * from "./fields/IFieldRelation";
+export * from "./fields/IFieldSerial";
+export * from "./fields/IFieldString";
 export declare type IFieldsStorageType = "string" | "number" | "boolean" | "date" | "reference" | "object" | "any";
 export declare type IFieldsFieldType = "string" | "number" | "boolean" | "date" | "dateTime" | "password" | "enumerate" | "document" | "block" | "relation" | "serial" | "any";
 export declare type IFieldsFieldMultiple = {
--- dist/IFields.js
+++ dist/IFields.js
@@ -1,3 +1,24 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./fields/IFieldBlock"), exports);
+__exportStar(require("./fields/IFieldBoolean"), exports);
+__exportStar(require("./fields/IFieldDate"), exports);
+__exportStar(require("./fields/IFieldDateTime"), exports);
+__exportStar(require("./fields/IFieldDocument"), exports);
+__exportStar(require("./fields/IFieldEnumerate"), exports);
+__exportStar(require("./fields/IFieldNumber"), exports);
+__exportStar(require("./fields/IFieldPassword"), exports);
+__exportStar(require("./fields/IFieldRelation"), exports);
+__exportStar(require("./fields/IFieldSerial"), exports);
+__exportStar(require("./fields/IFieldString"), exports);
 //# sourceMappingURL=IFields.js.map
