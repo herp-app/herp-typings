cb05ba9a2d5209679ce0d7fd8423804c68356056
Johannes Hoffmann <hoppe@juhujuhu.de>
2020-10-30 11:37:42 +0100

    use reference in indes type decleration

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,15 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./IBundle"), exports);
-__exportStar(require("./IConfiguration"), exports);
-__exportStar(require("./IEntity"), exports);
-__exportStar(require("./IFields"), exports);
-__exportStar(require("./IForm"), exports);
-__exportStar(require("./ILayout"), exports);
-__exportStar(require("./IMenu"), exports);
-__exportStar(require("./IPermissions"), exports);
-__exportStar(require("./IQuery"), exports);
-__exportStar(require("./IService"), exports);
-__exportStar(require("./ITranslation"), exports);
-__exportStar(require("./IView"), exports);
-__exportStar(require("./IWorkflows"), exports);
-__exportStar(require("./Utils"), exports);
+/// <reference path="./IBundle.ts" />
+/// <reference path="./IConfiguration.ts" />
+/// <reference path="./IEntity.ts" />
+/// <reference path="./IFields.ts" />
+/// <reference path="./IForm.ts" />
+/// <reference path="./ILayout.ts" />
+/// <reference path="./IMenu.ts" />
+/// <reference path="./IPermissions.ts" />
+/// <reference path="./IQuery.ts" />
+/// <reference path="./IService.ts" />
+/// <reference path="./ITranslation.ts" />
+/// <reference path="./IView.ts" />
+/// <reference path="./IWorkflows.ts" />
+/// <reference path="./Utils.ts" />
 //# sourceMappingURL=index.js.map
